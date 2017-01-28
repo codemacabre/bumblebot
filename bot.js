f4c5fcbbe60daa448437a2e206fb2c0604c4d6da
CodeMacabre <codemacabre@users.noreply.github.com>
2017-01-28 18:45:31 +0000

    Replace double quotes with single

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
-console.log("BumbleBot is starting...");
+console.log('BumbleBot is starting...');
 
 // Ensure NPM packages are installed
-var Twit = require("twit");
-var config = require("./config");
-var exec = require("child_process").exec;
-var fs = require("fs");
+var Twit = require('twit');
+var config = require('./config');
+var exec = require('child_process').exec;
+var fs = require('fs');
 
 var T = new Twit(config);
 
@@ -22,39 +22,39 @@ function loop() {
 
 function tweetIt() {
   // Path to Processing sketch
-  var cmd = "bumblebot/bumblebot";
+  var cmd = 'bumblebot/bumblebot';
   exec(cmd, processing);
 
   function processing() {
-    var imgFilename = "bumblebot/output.png"; // Path to image file output from Processing
+    var imgFilename = 'bumblebot/output.png'; // Path to image file output from Processing
     var imgParams = {
-      encoding: "base64"
+      encoding: 'base64'
     }
     var imgBee = fs.readFileSync(imgFilename, imgParams); // Read image
 
-    // var txtFilename = "bumblebot/output.txt"; // Path to text file output from Processing
+    // var txtFilename = 'bumblebot/output.txt'; // Path to text file output from Processing
     // var txtParams = {
-    //   encoding: "utf8"
+    //   encoding: 'utf8'
     // }
     // var txtBee = fs.readFileSync(txtFilename, txtParams); // Read text
 
 
-    T.post("media/upload", { media_data: imgBee }, uploaded); // Upload image before tweeting
+    T.post('media/upload', { media_data: imgBee }, uploaded); // Upload image before tweeting
 
     function uploaded(err, data, response) {
       var id = data.media_id_string; // Read uploaded image
       var tweet = {
-        status:     "", // Tweet text content
+        status:     '', // Tweet text content
         media_ids:  [id] // Tweet image content
       }
-      T.post("statuses/update", tweet, tweeted); // Send tweet
+      T.post('statuses/update', tweet, tweeted); // Send tweet
     }
 
     function tweeted(err, data, response) {
       if (err) {
-        console.log("Error! Tweet not sent.");
+        console.log('Error! Tweet not sent.');
       } else {
-        console.log("Success! Tweet sent.");
+        console.log('Success! Tweet sent.');
       }
     }
   }
